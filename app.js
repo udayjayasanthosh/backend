171f411c812aa660dbfaa89ddff227f4ddae143d
udayjayasanthosh <udayjayasanthosh0908@gmail.com>
2025-02-22 11:15:19 +0530

    third push

--- app.js
+++ app.js
@@ -1,36 +1,40 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-import { Inserting,Updating,Deleting,Getting_data,FindingBydate } from "./Controller/dairy_controller.js";
-import {Otp_generator,Login_check,Signup,Deleting_Account,Updating_password} from "./Controller/otp_controller.js";
-const app=express()
-const Router=express.Router()
+import { Inserting, Updating, Deleting, Getting_data, FindingBydate } from "./Controller/dairy_controller.js";
+import { Otp_generator, Login_check, Signup, Deleting_Account, Updating_password } from "./Controller/otp_controller.js";
+const app = express()
+const Router = express.Router()
 app.use(express.json())
-app.use("/",Router);
+app.use("/", Router);
 app.use(cors())
-const password=encodeURIComponent('UDAY0908');
+const password = encodeURIComponent('UDAY0908');
 const url = `mongodb+srv://22a91a0515:${password}@cluster0.w3ges.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-Router.post("/inserting",Inserting);
-Router.post("/updating",Updating);
-Router.post("/deleting",Deleting);
-Router.get("/getting/:user",Getting_data);
-Router.get("/gettingBydate/:date/:user",FindingBydate);
-Router.post("/otp_generated",Otp_generator);
-Router.post("/login",Login_check);
-Router.post("/signup",Signup);
-Router.post("/updating_pass",Updating_password);
-Router.get("/Deleting_account/:email",Deleting_Account);
+Router.post("/inserting", Inserting);
+Router.post("/updating", Updating);
+Router.post("/deleting", Deleting);
+Router.get("/getting/:user", Getting_data);
+Router.get("/gettingBydate/:date/:user", FindingBydate);
+Router.post("/otp_generated", Otp_generator);
+Router.post("/login", Login_check);
+Router.post("/signup", Signup);
+Router.post("/updating_pass", Updating_password);
+Router.get("/Deleting_account/:email", Deleting_Account);
 
 mongoose.connect(url)
-.then(()=>{
-    console.log("connection established");
-})
-.catch((err)=>{
-    console.log(err);
-})
+    .then(() => {
+        console.log("connection established");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
 
-app.listen(8080,()=>{
+app.listen(8080, () => {
     console.log("server started");
+    setInterval(() => {
+        fetch("https://backend-u7bo.onrender.com")
+            .catch(error => console.error("Error fetching data:", error));
+    }, 840000);
 })
 
